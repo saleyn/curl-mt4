5f7734031728fb4d573256415cf94b0da1d02bb3
Serge Aleynikov <saleyn@gmail.com>
2019-01-06 11:12:26 -0500

    Cleanup

--- curl-mt4/MT4/inet-curl.mqh
+++ curl-mt4/MT4/inet-curl.mqh
@@ -31,23 +31,50 @@ enum CURL_METHOD {
 //| DLL imports                                                      |
 //+------------------------------------------------------------------+
 #import "curl-mt4.dll"
-  int   CurlInit();
-  void  CurlFinalize   (int handle);
+  int   CurlInit();                     ///< Initialize CURL library
+  void  CurlFinalize   (int handle);    ///< Finalize CURL library
 
+  /// Set URL prior to calling `CurlExecute()`
   int   CurlSetURLW    (int handle, string url);
+  
+  /// Set request timeout in seconds
   int   CurlSetTimeout (int handle, int timeout_secs);
+
+  /// Add a single request header
   void  CurlAddHeaderW (int handle, string header);
-  void  CurlAddHeadersW(int handle, string headers); // '\n' delimited headers
+
+  /// Add '\n' delimited request headers
+  void  CurlAddHeadersW(int handle, string headers);
+
+  /// Execute a request on the server
+  /// @param code       resulting code (optional if passed nullptr) returned by the server (200 = success)
+  /// @param res_length resulting response body length (optional if passed nullptr)
+  /// @param opts       request options (OR'd CurlOptions)
+  /// @param post_data  request body for POST requests
   int   CurlExecuteW   (int handle, int& code, int& res_length, CURL_METHOD method=CURL_GET,
                         unsigned int opts=0, string post_data=NULL);
+
+  /// Return response body length
   int   CurlGetDataSize(int handle);
+
+  /// Return response data, where `buf` size must be pre-allocated to `res_length`
+  /// returned by `CurlExecute()`. Note that `CurlGetData()` function doesn't create an
+  /// extra copy of data compared to `CurlGetDataW()`.
   int   CurlGetData    (int handle, char& buf[], int size);
   int   CurlGetDataW   (int handle, string& buf, int size);
+
+  /// Get description of the `err` code
   int   CurlLastErrorW (int handle, int err, string& errs, int max_size);
 
-  void  CurlDbgLevel   (int handle, int level);
+  void  CurlDbgLevel   (int handle, int level);  //< Set debug level
+
+  /// Return size of buffer needed to fetch debug info
   int   CurlDbgInfoSize(int handle);
+
+  /// Return debug info where `buf` size must be pre-allocated to length
+  /// returned by `CurlDbgInfoSize()`
   void  CurlDbgInfoW   (int handle, string& buf, int size);
+
 #import
 //+------------------------------------------------------------------+
 
--- curl-mt4/curl-mt4.h
+++ curl-mt4/curl-mt4.h
@@ -1,5 +1,13 @@
+//------------------------------------------------------------------------------
+/// \file      curl-mt4.h
+/// \author    Serge Aleynikov
+/// \copyright (c) 2018, Serge Aleynikov
+//------------------------------------------------------------------------------
+/// \brief     Defines the API of calling `libcurl` functions from `MT4`
+//------------------------------------------------------------------------------
 #pragma once
 
+// When building curl-mt4.dll, add BUILDING_MT4CURL pre-processor define
 #ifdef BUILDING_MT4CURL
     #define MT4EXPORT __declspec(dllexport)
 #else
@@ -22,32 +30,65 @@ extern "C" {
         PUT,
     };
 
-    MT4EXPORT void* __stdcall CurlInit();
-    MT4EXPORT void  __stdcall CurlFinalize(void* handle);
+    using CurlHandle = void*;
+    using uint       = unsigned int;
 
-    MT4EXPORT int   __stdcall CurlSetURL     (void* handle, const char* url);
-    MT4EXPORT int   __stdcall CurlSetTimeout (void* handle, int timeout_secs);
-    MT4EXPORT void  __stdcall CurlAddHeaders (void* handle, const char* headers);
-    MT4EXPORT void  __stdcall CurlAddHeader  (void* handle, const char* header);
-    MT4EXPORT int   __stdcall CurlExecute    (void* handle, int* code, int* res_length, CurlMethod method=CurlMethod::GET,
-                                              unsigned int opts=0, const char* post_data=nullptr);
-    MT4EXPORT int   __stdcall CurlGetDataSize(void* handle);
-    MT4EXPORT int   __stdcall CurlGetData    (void* handle, char* buf, int size);
-    MT4EXPORT int   __stdcall CurlLastError  (void* handle, int err, char* errs, int max_size);
+    /// Initialize CURL library
+    MT4EXPORT CurlHandle __stdcall CurlInit();
+    /// Finalize CURL library
+    MT4EXPORT void       __stdcall CurlFinalize(CurlHandle handle);
 
-    MT4EXPORT void  __stdcall CurlDbgLevel   (void* handle, int level);
-    MT4EXPORT int   __stdcall CurlDbgInfoSize(void* handle);
-    MT4EXPORT int   __stdcall CurlDbgInfo    (void* handle, char* buf, int size);
+    /// Set URL prior to calling `CurlExecute()`
+    MT4EXPORT int        __stdcall CurlSetURL     (CurlHandle handle, const char* url);
+    /// Set request timeout in seconds
+    MT4EXPORT int        __stdcall CurlSetTimeout (CurlHandle handle, int timeout_secs);
+    /// Add '\n' delimited request headers
+    MT4EXPORT void       __stdcall CurlAddHeaders (CurlHandle handle, const char* headers);
+    /// Add a single request header
+    MT4EXPORT void       __stdcall CurlAddHeader  (CurlHandle handle, const char* header);
+    /// Execute a request on the server
+    /// @param code       resulting code (optional if passed nullptr) returned by the server (200 = success)
+    /// @param res_length resulting response body length (optional if passed nullptr)
+    /// @param opts       request options (OR'd CurlOptions)
+    /// @param post_data  request body for POST requests
+    MT4EXPORT int        __stdcall CurlExecute    (CurlHandle handle, int* code, int* res_length,
+                                                   CurlMethod method=GET,
+                                                   uint opts=uint(OPT_NONE), const char* post_data=nullptr);
+    /// Return response body length
+    MT4EXPORT int        __stdcall CurlGetDataSize(CurlHandle handle);
+    /// Return response data, where `buf` size must be pre-allocated to `res_length`
+    /// returned by `CurlExecute()`
+    MT4EXPORT int        __stdcall CurlGetData    (CurlHandle handle, char* buf, int size);
+    /// Get description of the `err` code
+    MT4EXPORT int        __stdcall CurlLastError  (CurlHandle handle, int err, char* errs, int max_size);
+    /// Set debug level
+    MT4EXPORT void       __stdcall CurlDbgLevel   (CurlHandle handle, int level);
+    /// Return size of buffer needed to fetch debug info
+    MT4EXPORT int        __stdcall CurlDbgInfoSize(CurlHandle handle);
+    /// Return debug info where `buf` size must be pre-allocated to length returned by `CurlDbgInfoSize()`
+    MT4EXPORT int        __stdcall CurlDbgInfo    (CurlHandle handle, char* buf, int size);
 
 #ifndef NO_CURLMT4_UNICODE_API
-    MT4EXPORT int   __stdcall CurlSetURLW    (void* handle, const wchar_t* url);
-    MT4EXPORT void  __stdcall CurlAddHeadersW(void* handle, const wchar_t* headers);
-    MT4EXPORT void  __stdcall CurlAddHeaderW (void* handle, const wchar_t* header);
-    MT4EXPORT int   __stdcall CurlExecuteW   (void* handle, int* code, int* res_length, CurlMethod method = CurlMethod::GET,
-                                              unsigned int opts = 0, const wchar_t* post_data = nullptr);
-    MT4EXPORT int   __stdcall CurlGetDataW   (void* handle, wchar_t* buf, int size);
-    MT4EXPORT int   __stdcall CurlLastErrorW (void* handle, int err, wchar_t* errs, int max_size);
-    MT4EXPORT int   __stdcall CurlDbgInfoW   (void* handle, wchar_t* buf, int size);
+    /// Set URL prior to calling `CurlExecute()`
+    MT4EXPORT int        __stdcall CurlSetURLW    (CurlHandle handle, const wchar_t* url);
+    /// Add '\n' delimited request headers
+    MT4EXPORT void       __stdcall CurlAddHeadersW(CurlHandle handle, const wchar_t* headers);
+    /// Add a single request header
+    MT4EXPORT void       __stdcall CurlAddHeaderW (CurlHandle handle, const wchar_t* header);
+    /// Execute a request on the server
+    /// @param code       resulting code (optional if passed nullptr) returned by the server (200 = success)
+    /// @param res_length resulting response body length (optional if passed nullptr)
+    /// @param opts       request options (OR'd CurlOptions)
+    /// @param post_data  request body for POST requests
+    MT4EXPORT int        __stdcall CurlExecuteW   (CurlHandle handle, int* code, int* res_length,
+                                                   CurlMethod method = GET,
+                                                   unsigned int opts = 0, const wchar_t* post_data = nullptr);
+    /// Return response data, where `buf` size must be pre-allocated to `res_length` returned by `CurlExecute()`
+    MT4EXPORT int        __stdcall CurlGetDataW   (CurlHandle handle, wchar_t* buf, int size);
+    /// Get description of the `err` code
+    MT4EXPORT int        __stdcall CurlLastErrorW (CurlHandle handle, int err, wchar_t* errs, int max_size);
+    /// Return debug info where `buf` size must be pre-allocated to length returned by `CurlDbgInfoSize()`
+    MT4EXPORT int        __stdcall CurlDbgInfoW   (CurlHandle handle, wchar_t* buf, int size);
 #endif
 
 } // extern
