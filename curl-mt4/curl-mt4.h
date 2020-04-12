109f901a748a366eb96d6d9cd4e07f4fed9c5505
Serge Aleynikov <saleyn@gmail.com>
2020-04-12 15:53:32 -0400

    Add 64bit build

--- curl-mt4/curl-mt4.h
+++ curl-mt4/curl-mt4.h
@@ -8,10 +8,23 @@
 #pragma once
 
 // When building curl-mt4.dll, add BUILDING_MT4CURL pre-processor define
-#ifdef BUILDING_MT4CURL
-    #define MT4EXPORT __declspec(dllexport)
+#if (defined(_WIN32)||defined(_WIN64)) && !defined(BUILDING_MT4CURL_STATIC)
+  #ifndef WINAPI
+  #define WINAPI __stdcall
+  #endif
+
+  #ifdef BUILDING_MT4CURL_DLL
+    #define APIEXPORT(R) __declspec(dllexport) R WINAPI
+  #else
+    #define APIEXPORT(R) __declspec(dllimport) R WINAPI
+  #endif
 #else
-    #define MT4EXPORT __declspec(dllimport)
+  #ifndef APIEXPORT
+    #define APIEXPORT(R) R WINAPI
+  #endif
+  #ifndef WINAPI
+    #define WINAPI
+  #endif
 #endif
 
 extern "C" {
@@ -23,7 +36,7 @@ extern "C" {
         OPT_DEBUG            = 1 << 2,
     };
 
-    enum CurlMethod : int {
+    enum class CurlMethod : int {
         GET,
         POST,
         POST_JSON,
@@ -36,74 +49,74 @@ extern "C" {
     using uint       = unsigned int;
 
     /// Initialize CURL library
-    MT4EXPORT CurlHandle __stdcall CurlInit();
+    APIEXPORT(CurlHandle)  CurlInit();
     /// Finalize CURL library
-    MT4EXPORT void       __stdcall CurlFinalize(CurlHandle handle);
+    APIEXPORT(void)        CurlFinalize(CurlHandle handle);
 
     /// Set URL prior to calling `CurlExecute()`
-    MT4EXPORT int        __stdcall CurlSetURL     (CurlHandle handle, const char* url);
+    APIEXPORT(int)         CurlSetURL     (CurlHandle handle, const char* url);
     /// Set request timeout in seconds
-    MT4EXPORT int        __stdcall CurlSetTimeout (CurlHandle handle, int timeout_secs);
+    APIEXPORT(int)         CurlSetTimeout (CurlHandle handle, int timeout_secs);
     /// Add '\n' delimited request headers
-    MT4EXPORT void       __stdcall CurlAddHeaders (CurlHandle handle, const char* headers);
+    APIEXPORT(void)        CurlAddHeaders (CurlHandle handle, const char* headers);
     /// Add a single request header
-    MT4EXPORT void       __stdcall CurlAddHeader  (CurlHandle handle, const char* header);
+    APIEXPORT(void)        CurlAddHeader  (CurlHandle handle, const char* header);
     /// Execute a request on the server
     /// @param code       resulting code (optional if passed nullptr) returned by the server (200 = success)
     /// @param res_length resulting response body length (optional if passed nullptr)
     /// @param opts       request options (OR'd CurlOptions)
     /// @param post_data  request body for POST requests
-    MT4EXPORT int        __stdcall CurlExecute    (CurlHandle handle, int* code, int* res_length,
-                                                   CurlMethod method=GET,
-                                                   uint opts=uint(OPT_NONE), const char* post_data=nullptr,
-                                                   int timeout_secs=10);
+    APIEXPORT(int)         CurlExecute    (CurlHandle handle, int* code, int* res_length,
+                                           CurlMethod method=CurlMethod::GET,
+                                           unsigned int opts=uint(OPT_NONE), const char* post_data=nullptr,
+                                           int timeout_secs=10);
     /// Return response body length
-    MT4EXPORT int        __stdcall CurlGetDataSize(CurlHandle handle);
+    APIEXPORT(int)         CurlGetDataSize(CurlHandle handle);
     /// Return response data, where `buf` size must be pre-allocated to `res_length`
     /// returned by `CurlExecute()`
-    MT4EXPORT int        __stdcall CurlGetData    (CurlHandle handle, char* buf, int size);
+    APIEXPORT(int)         CurlGetData    (CurlHandle handle, char* buf, int size);
     /// Get count of response headers
-    MT4EXPORT size_t    __stdcall  CurlTotRespHeaders(CurlHandle handle);
+    APIEXPORT(size_t)      CurlTotRespHeaders(CurlHandle handle);
     /// Get `idx`th response header.
     /// If the header's length is greater than `buflen`, the function doesn't update `buf`.
     /// Return the actual length of the header or -1 if `idx` is invalid.
-    MT4EXPORT int        __stdcall CurlGetRespHeader(void* handle, int idx, char* key, size_t buflen);
+    APIEXPORT(int)         CurlGetRespHeader(CurlHandle handle, int idx, char* key, size_t buflen);
     /// Get description of the `err` code
-    MT4EXPORT int        __stdcall CurlLastError  (CurlHandle handle, int err, char* errs, int max_size);
+    APIEXPORT(int)         CurlLastError  (CurlHandle handle, int err, char* errs, int max_size);
     /// Set debug level
-    MT4EXPORT void       __stdcall CurlDbgLevel   (CurlHandle handle, int level);
+    APIEXPORT(void)        CurlDbgLevel   (CurlHandle handle, int level);
     /// Return size of buffer needed to fetch debug info
-    MT4EXPORT int        __stdcall CurlDbgInfoSize(CurlHandle handle);
+    APIEXPORT(int)         CurlDbgInfoSize(CurlHandle handle);
     /// Return debug info where `buf` size must be pre-allocated to length returned by `CurlDbgInfoSize()`
-    MT4EXPORT int        __stdcall CurlDbgInfo    (CurlHandle handle, char* buf, int size);
+    APIEXPORT(int)         CurlDbgInfo    (CurlHandle handle, char* buf, int size);
 
 #ifndef NO_CURLMT4_UNICODE_API
     /// Set URL prior to calling `CurlExecute()`
-    MT4EXPORT int        __stdcall CurlSetURLW    (CurlHandle handle, const wchar_t* url);
+    APIEXPORT(int)         CurlSetURLW    (CurlHandle handle, const wchar_t* url);
     /// Add '\n' delimited request headers
-    MT4EXPORT void       __stdcall CurlAddHeadersW(CurlHandle handle, const wchar_t* headers);
+    APIEXPORT(void)        CurlAddHeadersW(CurlHandle handle, const wchar_t* headers);
     /// Add a single request header
-    MT4EXPORT void       __stdcall CurlAddHeaderW (CurlHandle handle, const wchar_t* header);
+    APIEXPORT(void)        CurlAddHeaderW (CurlHandle handle, const wchar_t* header);
     /// Get `idx`th response header.
     /// If the header's length is greater than `buflen`, the function doesn't update `buf`.
     /// Return the actual length of the header or -1 if `idx` is invalid.
-    MT4EXPORT int       __stdcall CurlGetRespHeaderW(CurlHandle handle, int idx, wchar_t* buf, size_t buflen);
+    APIEXPORT(int)        CurlGetRespHeaderW(CurlHandle handle, int idx, wchar_t* buf, size_t buflen);
 
     /// Execute a request on the server
     /// @param code       resulting code (optional if passed nullptr) returned by the server (200 = success)
     /// @param res_length resulting response body length (optional if passed nullptr)
     /// @param opts       request options (OR'd CurlOptions)
     /// @param post_data  request body for POST requests
-    MT4EXPORT int        __stdcall CurlExecuteW   (CurlHandle handle, int* code, int* res_length,
-                                                   CurlMethod method = GET,
+    APIEXPORT(int)         CurlExecuteW   (CurlHandle handle, int* code, int* res_length,
+                                                   CurlMethod method = CurlMethod::GET,
                                                    unsigned int opts = 0, const wchar_t* post_data = nullptr,
                                                    int  timeout_secs = 10);
     /// Return response data, where `buf` size must be pre-allocated to `res_length` returned by `CurlExecute()`
-    MT4EXPORT int        __stdcall CurlGetDataW   (CurlHandle handle, wchar_t* buf, int size);
+    APIEXPORT(int)         CurlGetDataW   (CurlHandle handle, wchar_t* buf, int size);
     /// Get description of the `err` code
-    MT4EXPORT int        __stdcall CurlLastErrorW (CurlHandle handle, int err, wchar_t* errs, int max_size);
+    APIEXPORT(int)         CurlLastErrorW (CurlHandle handle, int err, wchar_t* errs, int max_size);
     /// Return debug info where `buf` size must be pre-allocated to length returned by `CurlDbgInfoSize()`
-    MT4EXPORT int        __stdcall CurlDbgInfoW   (CurlHandle handle, wchar_t* buf, int size);
+    APIEXPORT(int)         CurlDbgInfoW   (CurlHandle handle, wchar_t* buf, int size);
 #endif
 
 } // extern
